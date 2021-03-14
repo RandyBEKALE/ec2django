fc79629e16e5ac8f904f21fca42a4c89d4b0ee9f
Randy BEKALE <drmoussadjibekale@gmail.com>
2021-03-14 17:30:24 +0100

    the first commit of my site

---
+++ README.md
@@ -0,0 +1,2 @@
+my first django application 
+i deploy it on EC2
---
+++ monsite/myapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse      #gere les requetes https
+from django.template import loader
+# Create your views here.
+
+
+def index(request):
+    template = loader.get_template('index2.html')
+    #variable de Template
+    #dans un dictionnaire
+
+    data = {"name": "Randy BEKALE", "Age": 31, "materielinfo": ["mac", "pc", 'imprimant'],
+            "skill": "Ingénieur Vision par Ordinateur - Consultant DevOps - Développeur Python",}
+
+    return HttpResponse(template.render(data))
+
+
+def somme(request):
+
+    return HttpResponse("la somme de 12 + 12 = {}".format(12+12))
